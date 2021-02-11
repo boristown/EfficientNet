af527ff63c41b37516fb7fb218d843f19acd2231
AI Era <47687653+boristown@users.noreply.github.com>
2021-02-11 10:36:07 +0800

    export_model.bat

---
+++ models/official/efficientnet/export_model.bat
@@ -0,0 +1,7 @@
+cd D:\Documents\GitHub\EfficientNet\models\official\efficientnet
+python export_model.py ^
+--output_tflite= "D:\TPU\tflite" ^
+--model_name="efficientnet-bx" ^
+--ckpt_dir="D:\TPU\model" ^
+--output_saved_model_dir="D:\TPU\output" 
+pause
